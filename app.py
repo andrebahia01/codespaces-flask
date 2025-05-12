4eab4717fc8240f95ad71752aa8d75e2ca8a9426
andrebahia01 <andre_santana21@hotmail.com>
2025-05-12 23:43:12 +0000

    boa noite

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/mensagem")
+def mensagem():
+    return "boa noite turma ads"
